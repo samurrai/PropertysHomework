b3576696ee2e2268f05443c507b7e084f0f699bd
samurrai <samurrrai.r@gmail.com>
2019-01-27 10:57:06 +0600

    Домашняя работа по свойствам

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertysHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UniversalArray array = new UniversalArray(4);
+            array[3] = 4;
+            Console.WriteLine(array[3]);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UniversalArray.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertysHomework
+{
+    public class UniversalArray
+    {
+        private int[] arr;
+
+        public UniversalArray(int size)
+        {
+            arr = new int[size];
+        }
+
+        public int this[int i]
+        {
+            get
+            {
+                if (i >= 0 && i < arr.Length)
+                {
+                    return arr[i];
+                }
+                return arr[0];
+            }
+
+            set
+            {
+                if (i > 0 && i < arr.Length)
+                {
+                    arr[i] = value * value;
+                }
+            }
+        }
+    }
+}
